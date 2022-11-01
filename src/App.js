bcef4b3953dd3810cfcacaaf0d6d9037ad3b543c
yujin <miru93198@gmail.com>
2022-11-01 13:24:14 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { createGlobalStyle } from 'styled-components';
+import TodoTemplate from './component/TodoTemplate';
+import TodoHead from './component/TodoHead';
+const GlokbalStyle = createGlobalStyle`
+  body {
+    background: #e9ecef;
+  }
+`
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <GlokbalStyle />
+    <TodoTemplate>
+    <TodoHead />
+
+    </TodoTemplate>
+    
+    </>
   );
 }
 
---
+++ src/component/Todolist.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+function todolist() {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default todolist;
